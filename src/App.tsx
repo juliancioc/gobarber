197c73a88d6390f875b541f6c620b99457ab75ba
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-05-14 08:23:09 -0300

    add tooltip in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignUp from './pages/SignUp';
-// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
     <>
-        <SignUp />
+        {/* <SignUp /> */}
+        <SignIn />
         <GlobalStyle />
     </>
 )
